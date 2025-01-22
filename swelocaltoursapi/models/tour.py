fd04ff89dc5a6871964269afc19909fa31cc9258
Julien Smith <juliencain1933@gmail.com>
2025-01-21 20:08:09 -0600

    fixed tour model

--- swelocaltoursapi/models/tour.py
+++ swelocaltoursapi/models/tour.py
@@ -1,9 +1,11 @@
 from django.db import models 
 from django.core.validators import MinValueValidator
 from .location import Location
+from .user import User
 
 class Tour(models.Model):
  
+ user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tours' )
  image = models.URLField()
  price = models.DecimalField(max_digits=9, decimal_places=2, validators=[MinValueValidator(0.00)])
  location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='tours')
