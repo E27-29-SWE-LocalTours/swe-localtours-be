5a4f8469837f49335ee9fb6a8fe030b43ab43176
Julien Smith <juliencain1933@gmail.com>
2025-01-21 19:35:12 -0600

    added user model and fixed itinerary model

--- swelocaltoursapi/models/itinerary.py
+++ swelocaltoursapi/models/itinerary.py
@@ -1,8 +1,11 @@
 from django.db import models
 from .tour import Tour
+from .user import User
 
 
 class Itinerary(models.Model):
+    
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='itineraries' )
     tour = models.ForeignKey(Tour, on_delete=models.CASCADE, related_name='itineraries')
     date_id = models.DateTimeField()
     completed = models.BooleanField(default=False)
