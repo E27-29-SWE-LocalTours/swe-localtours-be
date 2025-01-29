58354367eb993559e8c106df63d54e473e7d066d
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2025-01-28 19:29:26 -0600

    Added views for User

--- swelocaltours/urls.py
+++ swelocaltours/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin  # Use Django's admin module
 from django.urls import path, include
 from rest_framework import routers
 from swelocaltoursapi.views.auth import check_user, register_user  # Import functions directly
-from swelocaltoursapi.views import LocationView, ItineraryView, TourView
+from swelocaltoursapi.views import LocationView, ItineraryView, TourView, UserView
 
 # Initialize router
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'locations', LocationView, 'location')
 router.register(r'itineraries', ItineraryView, 'itinerary')
 router.register(r'tours', TourView, 'tour')
+router.register(r'users', UserView, 'user')
 
 # Define URL patterns
 urlpatterns = [
