2a39a64648375ee654f7a0041bd4955f59689c18
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-02-14 22:45:40 -0600

    added domain name for deployed site to cors whitelist

--- swelocaltours/settings.py
+++ swelocaltours/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
 # THIS IS NEW
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    "https://swe-local-tours-g4rzg2h61-justinglotzs-projects.vercel.app",
 )
 
 MIDDLEWARE = [
